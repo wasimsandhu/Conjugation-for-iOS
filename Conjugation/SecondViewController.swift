6b9d00898bd40ad08adb4b2faba0e1eb8024608b
Wasim Sandhu <sandhuwasim@gmail.com>
2016-03-25 19:09:48 -0700

    Initial Commit

---
+++ Conjugation/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Conjugation
+//
+//  Created by Wasim Sandhu on 3/25/16.
+//  Copyright © 2016 Wasim Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
